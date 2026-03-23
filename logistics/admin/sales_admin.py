f65752df4c613575e8217d8353b42d6a94e5b138
Amr Shipl <amrshipl09@gmail.com>
2026-03-23 10:21:44 +0000

    Fix delivery dashboard navigation: linked add products and view offers screens

--- logistics/admin/sales_admin.py
+++ logistics/admin/sales_admin.py
@@ -2,31 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from ..models.sales_rep import SalesRepresentative
-from ..models.sales_manager import SalesManager
 
-# إعداد ظهور المندوب كـ Inline جوه صفحة المستخدم (User)
+# لو في Inline خليه يعرض الـ user بس لحد ما نتأكد من الحقول التانية
 class SalesRepresentativeInline(admin.StackedInline):
     model = SalesRepresentative
     can_delete = False
-    verbose_name_plural = 'بيانات المندوب'
-    # عرض الحقول المتاحة فقط في الموديل الحالي
-    fields = ('warehouse', 'target_amount') 
+    fields = ('user',) # شيلنا warehouse و target_amount مؤقتاً
 
-class CustomUserAdmin(UserAdmin):
-    inlines = (SalesRepresentativeInline,)
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_region')
-    
-    def get_region(self, instance):
-        # بنحاول نجيب المنطقة لو المندوب مربوط، وإلا نرجع فاضي
-        try:
-            return instance.salesrepresentative.warehouse.name
-        except:
-            return "-"
-    get_region.short_description = 'المخزن/الفرع'
-
-# تسجيل الأدمن للمناديب بشكل مستقل برضه لو حبيت
+# تعديل كلاس الـ Admin الأساسي للمناديب
 @admin.register(SalesRepresentative)
 class SalesRepresentativeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'warehouse', 'target_amount')
-    search_fields = ('user__username', 'warehouse__name')
+    # هنعرض الـ user بس عشان نتخطى خطأ الـ E108
+    list_display = ('user',) 
 
